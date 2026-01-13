6cda5c6e972f2ebc98d5e6bf8cfd0fc7cd2fa405
MenachemKazido <agbg7172@gmail.com>
2026-01-13 17:02:16 +0200

    finish service a

--- service_b/app/main.py
+++ service_b/app/main.py
@@ -1,36 +1,10 @@
-from fastapi import FastAPI , HTTPException
+from fastapi import FastAPI
 import uvicorn
-from schemas import Location
-from storage.crud import DbOperations
+from routes import router
 
-db = DbOperations()
+
 app = FastAPI()
-
-
-@app.post("/add_location")
-def add_location(location: Location):
-    try:
-        if db.add_location(location.model_dump()):
-            return {"The location has been successfully added":True}
-        else:
-            return {"The location already exists in the system":False}
-    except Exception as e:
-        print(e)
-        raise HTTPException(status_code=500)
-
-
-@app.get("/get_all_location")
-def get_all():
-    try:
-        return db.get_all_location()
-    except Exception as e:
-        print(e)
-        raise HTTPException(status_code=500)
+app.include_router(router, tags=["coordinates"])
 
 
 
-
-
-
-if __name__ == "__main__":
-    uvicorn.run(app,host="localhost",port=8000)
--- service_b/app/routes.py
+++ service_b/app/routes.py
@@ -1,2 +1,28 @@
-from fastapi import APIRouter
+from fastapi import APIRouter ,HTTPException
+from schemas import Location
+from storage.crud import DbOperations
+
+
+db = DbOperations()
+router = APIRouter()
+
+@router.post("/add_location")
+def add_location(location: Location):
+    try:
+        if db.add_location(location.model_dump()):
+            return {"The location has been successfully added":True}
+        else:
+            return {"The location already exists in the system":False}
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=500,detail='error')
+
+
+@router.get("/get_all_location")
+def get_all():
+    try:
+        return db.get_all_location()
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=500,detail='error')
 
